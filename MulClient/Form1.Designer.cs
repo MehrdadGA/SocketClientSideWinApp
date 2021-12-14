8d9a61ae7a848aa19fb3123fb84c0e6a9bd123c6
me.ghafari <me.ghafari@ME-GHAFARI>
2021-12-14 16:19:59 +0330

    Add project files.

---
+++ MulClient/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace MulClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.txt_Port = new System.Windows.Forms.TextBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.txt_Send = new System.Windows.Forms.TextBox();
+            this.lst_Message = new System.Windows.Forms.ListBox();
+            this.lblConnect = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.lblConnect);
+            this.panel1.Controls.Add(this.btn_Connect);
+            this.panel1.Controls.Add(this.txt_Port);
+            this.panel1.Controls.Add(this.txt_IP);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(459, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(327, 369);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port :";
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(50, 35);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(169, 20);
+            this.txt_IP.TabIndex = 2;
+            this.txt_IP.Text = "172.20.3.173";
+            // 
+            // txt_Port
+            // 
+            this.txt_Port.Location = new System.Drawing.Point(50, 78);
+            this.txt_Port.Name = "txt_Port";
+            this.txt_Port.Size = new System.Drawing.Size(169, 20);
+            this.txt_Port.TabIndex = 3;
+            this.txt_Port.Text = "5050";
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(50, 156);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(93, 55);
+            this.btn_Connect.TabIndex = 4;
+            this.btn_Connect.Text = "Connect";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(332, 12);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(93, 41);
+            this.btn_Send.TabIndex = 5;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // txt_Send
+            // 
+            this.txt_Send.Location = new System.Drawing.Point(12, 12);
+            this.txt_Send.Multiline = true;
+            this.txt_Send.Name = "txt_Send";
+            this.txt_Send.Size = new System.Drawing.Size(314, 41);
+            this.txt_Send.TabIndex = 6;
+            this.txt_Send.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Send_KeyPress);
+            // 
+            // lst_Message
+            // 
+            this.lst_Message.FormattingEnabled = true;
+            this.lst_Message.Location = new System.Drawing.Point(12, 59);
+            this.lst_Message.Name = "lst_Message";
+            this.lst_Message.Size = new System.Drawing.Size(314, 433);
+            this.lst_Message.TabIndex = 7;
+            // 
+            // lblConnect
+            // 
+            this.lblConnect.AutoSize = true;
+            this.lblConnect.Location = new System.Drawing.Point(64, 214);
+            this.lblConnect.Name = "lblConnect";
+            this.lblConnect.Size = new System.Drawing.Size(67, 13);
+            this.lblConnect.TabIndex = 8;
+            this.lblConnect.Text = "Not Connect";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSalmon;
+            this.ClientSize = new System.Drawing.Size(798, 510);
+            this.Controls.Add(this.lst_Message);
+            this.Controls.Add(this.txt_Send);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MulClient";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.TextBox txt_Port;
+        private System.Windows.Forms.TextBox txt_IP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.TextBox txt_Send;
+        private System.Windows.Forms.ListBox lst_Message;
+        private System.Windows.Forms.Label lblConnect;
+    }
+}
+
---
+++ MulClient/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//-----------------------------------------------
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MulClient
+{
+    public partial class Form1 : Form
+    {
+
+        public Socket socClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void getData()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] b = new byte[1024];
+                    int r = socClient.Receive(b);
+                    if (r > 0)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            lst_Message.Items.Add(Encoding.Unicode.GetString(b, 0, r));
+                        });
+                    }
+                }
+                catch (Exception)
+                {
+                    ;
+                }
+            }
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                socClient.Connect(new IPEndPoint(IPAddress.Parse(txt_IP.Text), int.Parse(txt_Port.Text)));
+                Thread tr = new Thread(getData);
+                tr.IsBackground = true;
+                tr.Start();
+                lblConnect.Text = "Connected";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("You,re Connected now");
+            }
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            byte[] b = Encoding.Unicode.GetBytes(Environment.MachineName + " > " + txt_Send.Text);
+            socClient.Send(b);
+            lst_Message.Items.Add(Environment.MachineName + " > " + txt_Send.Text);
+
+            txt_Send.Text = "";
+        }
+
+        private void txt_Send_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                btn_Send.Focus();
+            }
+        }
+    }
+}
